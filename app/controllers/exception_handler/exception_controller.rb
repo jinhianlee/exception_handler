4dd2fd0213c4bb7bf451e51e25ea922ba1563b27
Jin Hian Lee <jinhian.lee@evie.ai>
2015-06-19 16:47:14 +0800

    Force the callback class name to be a String to avoid issues when used with Spring (see https://github.com/rails/spring#class-reloading)

--- app/controllers/exception_handler/exception_controller.rb
+++ app/controllers/exception_handler/exception_controller.rb
@@ -80,9 +80,8 @@ module ExceptionHandler
       handled = false
       ecb=ExceptionHandler.config.callback
       
-      if (ecb.kind_of? Hash) && (ecb[:class] != nil)
-        ecb[:class] = ecb[:class].constantize if ecb[:class].kind_of? String
-        cb_response = ecb[:class].send(
+      if (ecb.kind_of? Hash) && (ecb[:class] != nil) && (ecb[:class].kind_of? String)
+        cb_response = ecb[:class].constantize.send(
           (ecb[:method] || :'exception_handler_callback'), {
             :env => env,
             :exception => @exception,
